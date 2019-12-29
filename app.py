af1e307bb1ecaf7fa690765ca5a505d186ab4fa8
cdalsoniii@gmail.com <cdalson@howard.edu>
2019-12-29 17:49:30 -0500

    remove gunicorn

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H4(children='US Agriculture Exports (2011)'),
+    html.H4(children='US Agriculture Exports (2011) Test'),
     generate_table(df)
 ])
 
